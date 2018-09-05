ac6da8ce6ede8cef5712b9b4162c481e81d73be5
congphuong <congphuong32@gmail.com>
2018-09-05 16:21:58 +0700

    Updated BridgeManager

--- android/src/main/java/com/github/alinz/reactnativewebviewbridge/WebViewBridgeManager.java
+++ android/src/main/java/com/github/alinz/reactnativewebviewbridge/WebViewBridgeManager.java
@@ -38,7 +38,11 @@ public class WebViewBridgeManager extends SimpleViewManager<WebViewBridgeManager
     private static final String REACT_CLASS = "RCTWebViewBridge";
 
     public static final int COMMAND_SEND_TO_BRIDGE = 101;
-    public static final int COMMAND_EVALUTEJS = 102;
+
+    public final static String HEADER_CONTENT_TYPE = "content-type";
+
+    private static final String MIME_TEXT_HTML = "text/html";
+    private static final String MIME_UNKNOWN = "application/octet-stream";
 
     @Override
     public String getName() {
@@ -52,7 +56,6 @@ public class WebViewBridgeManager extends SimpleViewManager<WebViewBridgeManager
         Map<String, Integer> commandsMap = new HashMap<>();
 
         commandsMap.put("sendToBridge", COMMAND_SEND_TO_BRIDGE);
-        commandsMap.put("evaluateJS", COMMAND_EVALUTEJS);
 
         return commandsMap;
     }
@@ -66,6 +69,8 @@ public class WebViewBridgeManager extends SimpleViewManager<WebViewBridgeManager
         webView.addJavascriptInterface(new JavascriptBridge(webView), "WebViewBridge");
         webView.setWebChromeClient(new WebChromeClient());
         webView.setWebViewClient(new ReactWebViewClient());
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setJavaScriptEnabled(true);
         return webView;
     }
 
@@ -76,12 +81,6 @@ public class WebViewBridgeManager extends SimpleViewManager<WebViewBridgeManager
         switch (commandId) {
             case COMMAND_SEND_TO_BRIDGE:
                 sendToBridge(root, args.getString(0));
-                Log.d("Send", "32");
-                break;
-            case COMMAND_EVALUTEJS:
-                Log.d("Send", "33");
-//                root.evaluateJavascript(args.getString(0), null);
-                evaluateJS(root, args.getString(0));
                 break;
             default:
                 //do nothing!!!!
@@ -91,6 +90,7 @@ public class WebViewBridgeManager extends SimpleViewManager<WebViewBridgeManager
     private void sendToBridge(WebView view, String script) {
 //        String script = "WebViewBridge.onMessage('" + message + "');";
 //        String scrip2 = "WebViewBridge.send(\"aa\")";
+        Log.d("console", script);
         WebViewBridgeManager.evaluateJavascript(view, script);
     }
 
@@ -198,6 +198,26 @@ public class WebViewBridgeManager extends SimpleViewManager<WebViewBridgeManager
         }
     }
 
+    public static Boolean responseRequiresJSInjection(Response response) {
+        // we don't want to inject JS into redirects
+        if (response.isRedirect()) {
+            Log.d("redirect", "true");
+            return false;
+        }
+
+        // ...okhttp appends charset to content type sometimes, like "text/html; charset=UTF8"
+        final String contentTypeAndCharset = response.header(HEADER_CONTENT_TYPE, MIME_UNKNOWN);
+        // ...and we only want to inject it in to HTML, really
+        return contentTypeAndCharset.startsWith(MIME_TEXT_HTML);
+    }
+
+    public static Boolean urlStringLooksInvalid(String urlString) {
+        return urlString == null ||
+                urlString.trim().equals("") ||
+                !(urlString.startsWith("http") && !urlString.startsWith("www")) ||
+                urlString.contains("|");
+    }
+
     public WebResourceResponse shouldInterceptRequest(WebResourceRequest request, Boolean onlyMainFrame, ReactWebView webView) {
         Uri url = request.getUrl();
         String urlStr = url.toString();
@@ -206,9 +226,12 @@ public class WebViewBridgeManager extends SimpleViewManager<WebViewBridgeManager
             return null;
         }
 
-//        if (urlStringLooksInvalid(urlStr)) {
-//            return null;
-//        }
+        if (urlStringLooksInvalid(urlStr)) {
+            Log.d("INVALID", urlStr);
+            return null;
+        }
+
+        Log.d("URL", urlStr);
 
         try {
             Request req = new Request.Builder()
@@ -224,9 +247,10 @@ public class WebViewBridgeManager extends SimpleViewManager<WebViewBridgeManager
 
             Response response = httpClient.newCall(req).execute();
 
-//            if (!responseRequiresJSInjection(response)) {
-//                return null;
-//            }
+            if (!responseRequiresJSInjection(response)) {
+                Log.d("JSINJECT", "true");
+                return null;
+            }
 
             InputStream is = response.body().byteStream();
             MediaType contentType = response.body().contentType();
